ab5f7ddcd2b52012417b56fde51f69b6d2d05a92
abhigit-design <asomani2612@gmail.com>
2025-02-10 11:41:44 -0500

    Update ai_test_generator.py

--- scripts/ai_test_generator.py
+++ scripts/ai_test_generator.py
@@ -48,7 +48,7 @@ def generate_tests():
     os.makedirs(tests_dir, exist_ok=True)
 
     # Save the generated test code to a file
-    test_filename = "HelloWorldTest.java"
+    test_filename = "HelloControllerTest.java"
     with open(os.path.join(tests_dir, test_filename), "w") as f:
         f.write(test_code)
 
