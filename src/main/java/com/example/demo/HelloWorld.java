526c6e7fd2913a269287f06f463788ebf528dff6
abhigit-design <asomani2612@gmail.com>
2025-02-10 12:36:30 -0500

    Update HelloWorld.java

--- src/main/java/com/example/demo/HelloWorld.java
+++ src/main/java/com/example/demo/HelloWorld.java
@@ -7,15 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
-}
-
 @RestController
 @RequestMapping("/api")
-class HelloWorld {
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class, args);
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
